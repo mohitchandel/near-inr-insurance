464130e122e9698a3e0185dbf49d492aeaf18833
Mohit Chandel <devmohit93@gmail.com>
2023-08-19 20:15:38 +0530

    fix: refactor code

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,11 +1,6 @@
-// Find all our documentation at https://docs.near.org
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::{log, near_bindgen};
 
-// Define the default message
-const DEFAULT_MESSAGE: &str = "Hello";
-
-// Define the contract structure
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct Contract {
@@ -19,14 +14,21 @@ pub struct Contract {
     able_to_claim: bool,
 }
 
-// Define the default, which automatically initializes the contract
 impl Default for Contract {
     fn default() -> Self {
-        // Self{message: DEFAULT_MESSAGE.to_string()}
+        Self {
+            insurance_type: String::new(),
+            time_period: String::new(),
+            user_id: 0,
+            user_name: String::new(),
+            user_wallet_address: String::new(),
+            user_email: String::new(),
+            user_phone: String::new(),
+            able_to_claim: false,
+        }
     }
 }
 
-// Implement the contract structure
 #[near_bindgen]
 impl Contract {
     pub fn buy_insurance(
@@ -57,27 +59,30 @@ impl Contract {
 
     pub fn get_insurance_details(&self, user_id: u128) -> String {
         if self.user_id == user_id {
-            return format!(
+            format!(
                 "Insurance Type: {}\nTime Period: {}\nUser ID: {}\nUser Name: {}\nUser Wallet Address: {}\nUser Email: {}\nUser Phone: {}\nAble to Claim: {}",
-                self.insurance_type, self.time_period, self.user_id, self.user_name, self.user_wallet_address, self.user_email, self.user_phone, self.able_to_claim
-            );
+                self.insurance_type,
+                self.time_period,
+                self.user_id,
+                self.user_name,
+                self.user_wallet_address,
+                self.user_email,
+                self.user_phone,
+                self.able_to_claim
+            )
         } else {
-            return "User ID does not match".to_string();
+            "User ID does not match".to_string()
         }
     }
 }
 
-/*
- * The rest of this file holds the inline tests for the code above
- * Learn more about Rust tests: https://doc.rust-lang.org/book/ch11-01-writing-tests.html
- */
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn buy_insurance() {
-        let contract = Contract::default();
+        let mut contract = Contract::default();
         contract.buy_insurance(
             "Car Insurance".to_string(),
             "1 year".to_string(),
@@ -87,6 +92,19 @@ mod tests {
             "johndoe@email.com".to_string(),
             "1234567890".to_string(),
         );
-        assert_eq!(contract.get_insurance_details(1), format!("Insurance Type: {}\nTime Period: {}\nUser ID: {}\nUser Name: {}\nUser Wallet Address: {}\nUser Email: {}\nUser Phone: {}\nAble to Claim: {}", "Car Insurance".to_string(), "1 year".to_string(), 1, "John Doe".to_string(), "0x1234567890".to_string(), "johndoe@email.com".to_string(),"1234567890".to_string()));
+        assert_eq!(
+            contract.get_insurance_details(1),
+            format!(
+                "Insurance Type: {}\nTime Period: {}\nUser ID: {}\nUser Name: {}\nUser Wallet Address: {}\nUser Email: {}\nUser Phone: {}\nAble to Claim: {}",
+                "Car Insurance",
+                "1 year",
+                1,
+                "John Doe",
+                "0x1234567890",
+                "johndoe@email.com",
+                "1234567890",
+                false
+            )
+        );
     }
 }
